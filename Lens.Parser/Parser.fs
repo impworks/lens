848b7b41d14037065aaba336ab5a9da5ab0265cb
ForNeVeR <neverthness@gmail.com>
2012-10-17 22:33:48 +0700

    Класс Parser.

---
+++ Lens.Parser/Parser.fs
@@ -0,0 +1,8 @@
+﻿namespace Lens.Parser
+
+open Lens.SyntaxTree
+open Lens.SyntaxTree.SyntaxTree
+
+type Parser() =
+    member this.Parse (source : string) : NodeBase seq =
+        raise <| new ParseException "Not implemented"
