e62c37d0ae7374d736373abe346da4461df79bef
Andrew Kourosh <mail@spark.ms>
2012-10-25 12:00:07 +0400

    ноды для обращения к элементам массива и членам класса

---
+++ Lens.SyntaxTree/SyntaxTree/Expressions/GetIndexNode.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lens.SyntaxTree.SyntaxTree.Expressions
+{
+	/// <summary>
+	/// A node representing a read-access to an array or list's value.
+	/// </summary>
+	public class GetIndexNode : NodeBase
+	{
+		/// <summary>
+		/// Expression to index.
+		/// </summary>
+		public NodeBase Expression { get; set; }
+
+		/// <summary>
+		/// The index value.
+		/// </summary>
+		public NodeBase Index { get; set; }
+
+		public override Type GetExpressionType()
+		{
+			if (m_ExpressionType != null)
+				return m_ExpressionType;
+
+			var exprType = Expression.GetExpressionType();
+			if (exprType.IsArray)
+			{
+				m_ExpressionType = exprType.GetElementType();
+			}
+			else if (exprType.IsGenericType)
+			{
+				var gt = exprType.GetGenericTypeDefinition();
+				var args = exprType.GetGenericArguments();
+				if (gt == typeof (List<>))
+					m_ExpressionType = args[0];
+				else if (gt == typeof (Dictionary<,>))
+					m_ExpressionType = args[1];
+			}
+			else
+			{
+				Error("Type '{0}' cannot be indexed.", exprType);
+			}
+
+			return m_ExpressionType;
+		}
+
+		public override void Compile()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Lens.SyntaxTree/SyntaxTree/Expressions/GetMemberNode.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Lens.SyntaxTree.Utils;
+
+namespace Lens.SyntaxTree.SyntaxTree.Expressions
+{
+	/// <summary>
+	/// A node representing read access to a member of a type, field or property.
+	/// </summary>
+	public class GetMemberNode : NodeBase
+	{
+		/// <summary>
+		/// Expression to access a dynamic member.
+		/// </summary>
+		public NodeBase Expression { get; set; }
+
+		/// <summary>
+		/// Type signature to access a static type.
+		/// </summary>
+		public TypeSignature StaticType { get; set; }
+
+		/// <summary>
+		/// The name of the member to access.
+		/// </summary>
+		public string MemberName { get; set; }
+
+		public override Type GetExpressionType()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Compile()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
