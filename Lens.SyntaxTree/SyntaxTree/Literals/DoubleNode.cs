4cd0c3487366097e01d154be5c630a2c6b91bd08
Andrew Kourosh <mail@spark.ms>
2012-10-27 22:57:02 +0400

    исправлено именование нод в пространстве имен Literals

---
+++ Lens.SyntaxTree/SyntaxTree/Literals/DoubleNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lens.SyntaxTree.SyntaxTree.Literals
+{
+	/// <summary>
+	/// A node representing floating point double-precision literals.
+	/// </summary>
+	public class DoubleNode : LiteralNodeBase<double>
+	{
+		public override void Compile()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Lens.SyntaxTree/SyntaxTree/Literals/StringNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lens.SyntaxTree.SyntaxTree.Literals
+{
+	/// <summary>
+	/// A node representing string literals.
+	/// </summary>
+	public class StringNode : LiteralNodeBase<string>
+	{
+		public override void Compile()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
