15a788b9ff7df7603b7d7207b2a6e4d113096812
Andrew Kourosh <mail@spark.ms>
2012-10-27 23:01:27 +0400

    RootNode

---
+++ Lens.SyntaxTree/SyntaxTree/RootNode.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Lens.SyntaxTree.SyntaxTree.ControlFlow;
+
+namespace Lens.SyntaxTree.SyntaxTree
+{
+	/// <summary>
+	/// The root node representing the whole program.
+	/// </summary>
+	public class RootNode : NodeBase
+	{
+		public RootNode()
+		{
+			UsingNamespaces = new List<string>();
+			Types = new List<TypeDefinitionNode>();
+			Records = new List<RecordDefinitionNode>();
+			Code = new CodeBlockNode();
+		}
+
+		/// <summary>
+		/// The namespaces resolved with the 'using' keyword.
+		/// </summary>
+		public List<string> UsingNamespaces { get; private set; }
+
+		/// <summary>
+		/// Declared types.
+		/// </summary>
+		public List<TypeDefinitionNode> Types { get; private set; }
+
+		/// <summary>
+		/// Declared records.
+		/// </summary>
+		public List<RecordDefinitionNode> Records { get; private set; }
+
+		/// <summary>
+		/// The code of the program.
+		/// </summary>
+		public CodeBlockNode Code { get; private set; }
+
+		public override Type GetExpressionType()
+		{
+			throw new InvalidOperationException("Root node!");
+		}
+
+		public override void Compile()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
