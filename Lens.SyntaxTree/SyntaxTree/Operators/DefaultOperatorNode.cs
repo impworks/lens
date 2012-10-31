3fd2c04338e42ce03bd0b7b63f1772a08eabfeca
Andrew Kourosh <mail@spark.ms>
2012-10-31 20:19:37 +0400

    оператор default

---
+++ Lens.SyntaxTree/SyntaxTree/Operators/DefaultOperatorNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lens.SyntaxTree.SyntaxTree.Operators
+{
+	/// <summary>
+	/// A node representing the operator that returns a default value for the type.
+	/// </summary>
+	public class DefaultOperatorNode : TypeOperatorNodeBase
+	{
+		public override Type GetExpressionType()
+		{
+			return Type.Type;
+		}
+
+		public override void Compile()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- Lens.SyntaxTree/SyntaxTree/Operators/TypeofOperatorNode.cs
+++ Lens.SyntaxTree/SyntaxTree/Operators/TypeofOperatorNode.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using Lens.SyntaxTree.Utils;
 
 namespace Lens.SyntaxTree.SyntaxTree.Operators
 {
 	/// <summary>
 	/// A node representing the typeof operator.
 	/// </summary>
-	public class TypeofOperatorNode : NodeBase
+	public class TypeofOperatorNode : TypeOperatorNodeBase
 	{
-		/// <summary>
-		/// Type to be inspected.
-		/// </summary>
-		public TypeSignature Type { get; set; }
-
 		public override Type GetExpressionType()
 		{
 			return typeof (Type);
@@ -22,27 +16,5 @@ namespace Lens.SyntaxTree.SyntaxTree.Operators
 		{
 			throw new NotImplementedException();
 		}
-
-		#region Equality members
-
-		protected bool Equals(TypeofOperatorNode other)
-		{
-			return Equals(Type, other.Type);
-		}
-
-		public override bool Equals(object obj)
-		{
-			if (ReferenceEquals(null, obj)) return false;
-			if (ReferenceEquals(this, obj)) return true;
-			if (obj.GetType() != this.GetType()) return false;
-			return Equals((TypeofOperatorNode)obj);
-		}
-
-		public override int GetHashCode()
-		{
-			return (Type != null ? Type.GetHashCode() : 0);
-		}
-
-		#endregion
 	}
 }
