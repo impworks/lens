a9a4d72e1a3cdb99c7dc5dcd628a6dab68b7583e
Andrew Kourosh <mail@spark.ms>
2013-01-03 01:25:37 +0400

    переименовал ParseException в LensCompilerException

---
+++ Lens.SyntaxTree/LensCompilerException.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Lens.SyntaxTree.Utils;
+
+namespace Lens.SyntaxTree
+{
+	/// <summary>
+	/// A generic exception that has occured during parse.
+	/// </summary>
+	public class LensCompilerException : Exception
+	{
+		public LensCompilerException(string msg) : base(msg)
+		{ }
+
+		public LensCompilerException(string msg, LocationEntity entity) : base(msg)
+		{
+			StartLocation = entity.StartLocation;
+			EndLocation = entity.EndLocation;
+		}
+
+		public LensCompilerException(string msg, LexemLocation start, LexemLocation end) : base(msg)
+		{
+			StartLocation = start;
+			EndLocation = end;
+		}
+
+		/// <summary>
+		/// Start of the erroneous segment.
+		/// </summary>
+		public LexemLocation StartLocation { get; private set; }
+
+		/// <summary>
+		/// End of the erroneous segment.
+		/// </summary>
+		public LexemLocation EndLocation { get; private set; }
+
+		/// <summary>
+		/// Full message with error positions.
+		/// </summary>
+		public string FullMessage
+		{
+			get
+			{
+				return string.Format(
+					"{0}\nLocation: {1}:{2} ... {3}:{4}",
+					Message,
+					StartLocation.Line,
+					StartLocation.Offset,
+					EndLocation.Line,
+					EndLocation.Offset
+				);
+			}
+		}
+	}
+}
