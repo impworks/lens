f55900371672695d787c658e404c2f9598ad3182
Andrew Kourosh <mail@spark.ms>
2013-01-04 22:06:40 +0400

    интерфейсы-маркеры и хелпер-метод

---
+++ Lens.SyntaxTree/Utils/MarkerInterfaces.cs
@@ -0,0 +1,17 @@
+﻿namespace Lens.SyntaxTree.Utils
+{
+	/// <summary>
+	/// A marker interface for all defined records.
+	/// </summary>
+	public interface ILensRecord { }
+
+	/// <summary>
+	/// A marker interface for all defined types.
+	/// </summary>
+	public interface ILensType { }
+
+	/// <summary>
+	/// A marker interface for all defined type labels.
+	/// </summary>
+	public interface ILensTypeRecord { }
+}
---
+++ Lens.SyntaxTree/Utils/TypeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lens.SyntaxTree.Utils
+{
+	public static class TypeExtensions
+	{
+		/// <summary>
+		/// Checks whether the type implements the interface
+		/// </summary>
+		/// <typeparam name="IType">Interface type.</typeparam>
+		/// <param name="type">Type to check.</param>
+		public static bool Implements<IType>(this Type type)
+		{
+			var info = typeof (IType);
+			return info.IsInterface && info.IsAssignableFrom(type);
+		}
+	}
+}
