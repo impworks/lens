46d094462fce5371ad13d29aa4197517c253d8db
ForNeVeR <neverthness@gmail.com>
2013-01-07 16:27:39 +0700

    Parser: добавил самодельную обработку ошибок.

---
+++ Lens.Parser/ErrorProvider.fs
@@ -0,0 +1,38 @@
+﻿module Lens.Parser.ErrorProvider
+
+open FParsec
+open FParsec.Error
+
+/// Set to false when debugging complex parser failures.
+let enabled = true
+
+let rec messageSeq (list : ErrorMessageList) : ErrorMessage seq =
+    match list with
+    | null -> Seq.empty
+    | _    -> Seq.concat [Seq.singleton list.Head
+                          messageSeq list.Tail]
+
+let rec private produceMessage (message : ErrorMessage) : string =
+    match message with
+    | Expected           label -> sprintf "Expected %s" label
+    | ExpectedString     str
+    | ExpectedStringCI   str   -> sprintf "Expected string %s" str
+    | Unexpected         label -> sprintf "Unexpected %s" label
+    | UnexpectedString   str
+    | UnexpectedStringCI str   -> sprintf "Unexpected string %s" str
+    | Message            str   -> sprintf "Exceptional message: %s" str
+    | NestedError(position, userState, errors)          -> sprintf "Nested error: %s"   <| produceErrorMessageList errors
+    | CompoundError(label, position, userState, errors) -> sprintf "Compound error: %s" <| produceErrorMessageList errors
+    | OtherErrorMessage  o     -> sprintf "Exceptional case: %A" o
+    | other                    -> sprintf "Unknown parse error: %A" other
+
+and private produceErrorMessageList (messages : ErrorMessageList) : string =
+    messageSeq messages
+    |> Seq.map produceMessage
+    |> String.concat "\n"
+
+let produce (message : string) (error : ParserError) (userState : ParserState) : string =
+    if enabled then
+        let position = error.Position
+        sprintf "At line %d, column %d: %s" position.Line position.Column <| produceErrorMessageList error.Messages
+    else message
--- Lens.Parser/TreeBuilder.fs
+++ Lens.Parser/TreeBuilder.fs
@@ -8,5 +8,5 @@ open Lens.SyntaxTree.SyntaxTree
 type TreeBuilder() =
     member this.Parse source : NodeBase seq =
         match runParserOnString Grammar.main (ParserState.Create()) "source" source with
-        | Success(result,  _, _) -> result :> NodeBase seq
-        | Failure(message, _, _) -> failwith message
+        | Success(result,  _, _)             -> result :> NodeBase seq
+        | Failure(message, error, userState) -> failwith <| ErrorProvider.produce message error userState
