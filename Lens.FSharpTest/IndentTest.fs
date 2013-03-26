28c88703ffe4f8045a7e25c3af2520fe52a920dd
ForNeVeR <neverthness@gmail.com>
2013-03-26 22:26:31 +0700

    Add test project.

---
+++ Lens.FSharpTest/IndentTest.fs
@@ -0,0 +1,25 @@
+﻿namespace Lens.FSharpTest
+
+open FParsec
+open NUnit.Framework
+
+open Lens.Parser
+open Lens.Parser.Indentation
+
+[<TestFixture>]
+type IndentTest() =
+    let testSuccess source parser state =
+        match runParserOnString parser (ParserState.Create()) "source" source with
+        | Success(_, userState, _) -> Assert.AreEqual(state, userState)
+        | other                    -> Assert.Fail <| sprintf "Parse failed: %A" other
+
+    [<Test>]
+    member this.SingleIndent() =
+        let parser = parse {
+            do! indent
+        }
+
+        let source = "
+    "
+        testSuccess source parser { RealIndentation = 1
+                                    VirtualIndentation = 1 }
