0825628ec81e3f2d67b13216ee9555104a062557
ForNeVeR <neverthness@gmail.com>
2013-04-06 23:30:35 +0700

    Add indentation test.

--- Lens.FSharpTest/IndentTest.fs
+++ Lens.FSharpTest/IndentTest.fs
@@ -52,3 +52,12 @@ type IndentTest() =
         let block = pchar 'b' .>>. indentedBlockOf (pchar 'x') .>>. pchar 'e' .>>. eof
         let source = "b\n    x\n    x\ne"
         testSuccess source block <| ParserState.Create()
+
+    [<Test>]
+    member this.TwoBlocks() =
+        let parser = pchar '1'
+                     .>>. indentedBlockOf (pchar '2' .>>. indentedBlockOf (pchar '3') .>>. pchar '2')
+                     .>>. pchar '1'
+                     .>>. eof
+        let source = "1\n    2\n        3\n    2\n1"
+        testSuccess source parser <| ParserState.Create()
