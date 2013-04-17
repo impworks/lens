a64fa2e692b8ea4ffff09559fc0b113996b24b23
ForNeVeR <neverthness@gmail.com>
2013-04-17 21:50:01 +0700

    Small rework of the error reporter.

--- Lens.Parser/ErrorProvider.fs
+++ Lens.Parser/ErrorProvider.fs
@@ -1,13 +1,16 @@
 ﻿module Lens.Parser.ErrorProvider
 
+open System
+
 open FParsec
 open FParsec.Error
 
 open Lens.SyntaxTree
 open Lens.SyntaxTree.SyntaxTree
+open Lens.SyntaxTree.Translations
 
 /// Set to false when debugging complex parser failures.
-let enabled = false
+let enabled = true
 
 let rec messageSeq (list : ErrorMessageList) : ErrorMessage seq =
     match list with
@@ -15,29 +18,41 @@ let rec messageSeq (list : ErrorMessageList) : ErrorMessage seq =
     | _    -> Seq.concat [Seq.singleton list.Head
                           messageSeq list.Tail]
 
-let rec private produceMessage (message : ErrorMessage) : string =
-    match message with
-    | Expected           label -> sprintf "Expected %s" label
-    | ExpectedString     str
-    | ExpectedStringCI   str   -> sprintf "Expected string %s" str
-    | Unexpected         label -> sprintf "Unexpected %s" label
-    | UnexpectedString   str
-    | UnexpectedStringCI str   -> sprintf "Unexpected string %s" str
-    | Message            str   -> sprintf "Exceptional message: %s" str
-    | NestedError(position, userState, errors)          -> sprintf "Nested error: %s"   <| produceErrorMessageList errors
-    | CompoundError(label, position, userState, errors) -> sprintf "Compound error: %s" <| produceErrorMessageList errors
-    | OtherErrorMessage  o     -> sprintf "Exceptional case: %A" o
-    | other                    -> sprintf "Unknown parse error: %A" other
+let rec private produceMessage (indent: int) (expectedWordWritten: bool) (message : ErrorMessage) : bool * string =
+    let expected = if not expectedWordWritten
+                   then "Expected "
+                   else "      or "
+    
+    // TODO: Filter string and lexem lists by unique values. Sort them.
+    let result = 
+        match message with
+        | NestedError(position, userState, errors)          ->
+            false, sprintf "Nested errors: %s"   <| produceErrorMessageList (indent + 1) errors
+        | CompoundError(label, position, userState, errors) ->
+            false, sprintf "Compound errors: %s" <| produceErrorMessageList (indent + 1) errors
+        | Expected           label -> true, expected + String.Format(CompilerMessages.LexemExpected, label)
+        | ExpectedString     str
+        | ExpectedStringCI   str   -> true, expected + String.Format(CompilerMessages.StringExpected, str)
+        | Unexpected         label -> false, sprintf "Unexpected %s" label
+        | UnexpectedString   str
+        | UnexpectedStringCI str   -> false, sprintf "Unexpected string '%s'" str
+        | Message            str   -> false, sprintf "Exceptional message: %s" str
+        | OtherErrorMessage  o     -> false, sprintf "Exceptional case: %A" o
+        | other                    -> false, sprintf "Unknown parse error: %A" other
+    let indentString = List.replicate indent "    " |> String.concat String.Empty
+    (fst result, indentString + snd result)
 
-and private produceErrorMessageList (messages : ErrorMessageList) : string =
-    messageSeq messages
-    |> Seq.map produceMessage
-    |> String.concat "\n"
+and private produceErrorMessageList (indent: int) (messages : ErrorMessageList) : string =
+    let msgSeq = messageSeq messages
+    let converter (expected, msg) error = let expected', msg' = produceMessage indent expected error
+                                          (expected || expected', msg + "\n" + msg')
+    let state = (false, String.Empty)
+    snd <| Seq.fold converter state msgSeq
 
 let private getMessage (message : string) (error : ParserError) (userState : ParserState) : string =
     if enabled then
         let position = error.Position
-        sprintf "At line %d, column %d: %s" position.Line position.Column <| produceErrorMessageList error.Messages
+        sprintf "At line %d, column %d: %s" position.Line position.Column <| produceErrorMessageList 0 error.Messages
     else message
 
 let getException message (error : ParserError) userState =
