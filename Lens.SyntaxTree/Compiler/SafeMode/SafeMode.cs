9b22995ed735bb49cb58bf3172ad9d74d93ce280
Andrew Kourosh <mail@spark.ms>
2013-09-03 22:28:56 +0400

    Стабы для безопасного режима.

---
+++ Lens.SyntaxTree/Compiler/SafeMode/SafeMode.cs
@@ -0,0 +1,20 @@
+﻿namespace Lens.SyntaxTree.Compiler.SafeMode
+{
+	public enum SafeMode
+	{
+		/// <summary>
+		/// All types and namespaces are allowed.
+		/// </summary>
+		Disabled,
+
+		/// <summary>
+		/// All types and namespaces are allowed except for explicitly specified ones.
+		/// </summary>
+		Blacklist,
+
+		/// <summary>
+		/// Only the explicitly specified types and namespaces are allowed.
+		/// </summary>
+		Whitelist
+	}
+}
