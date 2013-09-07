3d7e63f920a641c009d999860c64452ec7e41fdf
Andrew Kourosh <mail@spark.ms>
2013-09-07 16:29:44 +0400

    Тесты для остальных подсистем.

--- Lens.Test/SafeModeTest.cs
+++ Lens.Test/SafeModeTest.cs
@@ -143,13 +143,78 @@ GC::Collect ()
 			var src = @"System.Diagnostics.Debug::WriteLine ""test""";
 			testSubsystem(typeof(System.Diagnostics.Debug), SafeModeSubsystem.Environment, src);
 		}
-
-
+		
 		[Test]
 		public void BlacklistEnvironment5()
 		{
 			var src = @"System.Runtime.InteropServices.Marshal::IsComObject (new object ())";
 			testSubsystem(typeof(System.Runtime.InteropServices.Marshal), SafeModeSubsystem.Environment, src);
+		}
+
+		[Test]
+		public void BlacklistIO1()
+		{
+			var src = @"System.IO.Directory::Exists ""C:\A\B""";
+			testSubsystem(typeof(System.IO.Directory), SafeModeSubsystem.IO, src);
+		}
+
+		[Test]
+		public void BlacklistIO2()
+		{
+			var src = @"System.IO.IsolatedStorage.IsolatedStorageFile::IsEnabled";
+			testSubsystem(typeof(System.IO.IsolatedStorage.IsolatedStorageFile), SafeModeSubsystem.IO, src);
+		}
+
+		[Test]
+		public void BlacklistThreading1()
+		{
+			var src = @"
+var workThreads = 0
+var cpThreads = 0
+System.Threading.ThreadPool::GetAvailableThreads (ref workThreads) (ref cpThreads)";
+			testSubsystem(typeof(System.Threading.ThreadPool), SafeModeSubsystem.Threading, src);
+		}
+
+		[Test]
+		public void BlacklistThreading2()
+		{
+			var src = @"System.Threading.Tasks.Task::Run (-> print ""hello world!"")";
+			testSubsystem(typeof(System.Threading.Tasks.Task), SafeModeSubsystem.Threading, src);
+		}
+
+		[Test]
+		public void BlacklistReflection1()
+		{
+			var src = @"System.Reflection.Assembly::GetCallingAssembly()";
+			testSubsystem(typeof(System.Reflection.Assembly), SafeModeSubsystem.Reflection, src);
+		}
+
+		[Test]
+		public void BlacklistReflection2()
+		{
+			var src = @"System.AppDomain::CurrentDomain.IsFullyTrusted";
+			testSubsystem(typeof(AppDomain), SafeModeSubsystem.Reflection, src);
+		}
+
+		[Test]
+		public void BlacklistReflection3()
+		{
+			var src = @"(typeof int).Fullname";
+			testSubsystem(typeof(Type), SafeModeSubsystem.Reflection, src);
+		}
+
+		[Test]
+		public void BlacklistNetwork1()
+		{
+			var src = @"new System.Net.HttpListener ()";
+			testSubsystem(typeof(System.Net.HttpListener), SafeModeSubsystem.Network, src);
+		}
+
+		[Test]
+		public void BlacklistNetwork2()
+		{
+			var src = @"System.Net.Sockets.Socket::OSSupportsIPv4";
+			testSubsystem(typeof(System.Net.Sockets.Socket), SafeModeSubsystem.Network, src);
 		}
 
 		private void testSubsystem(Type type, SafeModeSubsystem system, string code)
