7dd81d5f2758d09eeb96de4f9015565ff445cb4e
Andrew Kourosh <mail@spark.ms>
2013-10-17 18:32:17 +0400

    Несколько базовых нод в парсере.

--- Lens/Parser/LensParser.cs
+++ Lens/Parser/LensParser.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using Lens.Compiler;
 using Lens.Lexer;
 using Lens.SyntaxTree;
+using Lens.SyntaxTree.ControlFlow;
 
 namespace Lens.Parser
 {
@@ -16,9 +20,91 @@ namespace Lens.Parser
 		{
 			Lexems = lexems.ToArray();
 
-			Nodes = new List<NodeBase>();
-
-			// todo : parse
+			Nodes = parseMain().ToList();
 		}
+
+		#region Globals
+
+		/// <summary>
+		/// main                                        = { stmt } EOF
+		/// </summary>
+		private IEnumerable<NodeBase> parseMain()
+		{
+			while (!peek(LexemType.EOF))
+				yield return parseStmt();
+
+			skip();
+		}
+
+		/// <summary>
+		/// stmt                                        = using | record_def | type_def | fun_def | local_stmt
+		/// </summary>
+		private NodeBase parseStmt()
+		{
+			return attempt(parseUsing)
+			       ?? attempt(parseRecordDef)
+			       ?? attempt(parseTypeDef)
+			       ?? attempt(parseFunDef)
+			       ?? ensure(parseLocalStmt, "Unknown kind of statement!");
+		}
+
+		#endregion
+
+		#region Namespace & type signatures
+
+		/// <summary>
+		/// namespace                                   = identifier { "." identifier }
+		/// </summary>
+		private TypeSignature parseNamespace()
+		{
+			return bind(() =>
+				{
+					if (!peek(LexemType.Identifier))
+						return null;
+
+					var identifier = getValue();
+					if (!peek(LexemType.Dot))
+						return new TypeSignature(identifier);
+
+					var sb = new StringBuilder(identifier);
+					while (check(LexemType.Dot))
+					{
+						identifier = ensure(LexemType.Identifier, "An identifier is expected!").Value;
+						sb.Append(".");
+						sb.Append(identifier);
+					}
+
+					return new TypeSignature(sb.ToString());
+				}
+			);
+		}
+
+		/// <summary>
+		/// type                                        = namespace [ type_args ] { "[]" }
+		/// </summary>
+		private TypeSignature parseType()
+		{
+			
+		}
+
+		#endregion
+
+		#region Structures
+
+		/// <summary>
+		/// using                                       = "using" namespace NL
+		/// </summary>
+		private UsingNode parseUsing()
+		{
+			if (!check(LexemType.Using))
+				return null;
+
+			var nsp = ensure(parseNamespace, "A namespace is expected!");
+			ensure(LexemType.NewLine, "A using statement should end with a newline!");
+
+			return new UsingNode {Namespace = nsp.FullSignature};
+		}
+
+		#endregion
 	}
 }
