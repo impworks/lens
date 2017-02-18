89f24f09d9802dbbf4e33376312862419fefd00f
Andrey Kurosh <mail@spark.ms>
2017-02-18 17:47:18 +0300

    Известные проблемы вынесены в отдельный список тестов для починки билда.

---
+++ Lens.Test/Features/KnownIssues.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace Lens.Test.Features
+{
+    /// <summary>
+    /// These tests contain descriptions of known problems which are not yet fixed.
+    /// Corresponding Github issues are also mentioned.
+    /// </summary>
+    [Ignore]
+    [TestFixture]
+    internal class KnownIssues: TestBase
+    {
+        /// <summary>
+        /// https://github.com/impworks/lens/issues/180
+        /// </summary>
+        [Test]
+        public void ClosureAndOrdinaryAccess()
+        {
+            var src = @"
+var x = 0
+while x < 5 do
+    (-> Console::WriteLine x) ()
+    x += 1
+";
+            Test(src, null);
+        }
+
+        /// <summary>
+        /// https://github.com/impworks/lens/issues/182
+        /// </summary>
+        [Test]
+        public void PatternMatchingBody()
+        {
+            var src = @"
+match new [1; 2; 3] with
+    case [x; ...y] then
+        var t = y.GetType ()
+        t.Name
+";
+            Test(src, "System.Int32[]");
+        }
+    }
+}
