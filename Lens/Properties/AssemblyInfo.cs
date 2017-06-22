6a116fa93b8ac97f23418a4fe8c8b259a3e9aa55
Friedrich von Never <friedrich@fornever.me>
2017-06-22 20:16:38 +0700

    Project: release 4.1.0

--- Lens/Properties/AssemblyInfo.cs
+++ Lens/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8c1a430f-cb27-4b42-8a08-f87f47f21663")]
-[assembly: AssemblyVersion("4.0.0.*")]
+[assembly: AssemblyVersion("4.1.0.*")]
 [assembly: InternalsVisibleTo("Lens.Test")]
